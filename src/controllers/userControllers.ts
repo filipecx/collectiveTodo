f73c84def9a78406146e132777596112d94610b0
filipecx <filipecavalcantiximenes@gmail.com>
2026-03-20 18:11:41 -0300

    added: basic controllers structure

---
+++ src/controllers/userControllers.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export async function create(req: Request, res: Response) {}
+
+export async function getAll(req: Request, res: Response) {}
+
+export async function get(req: Request, res: Response) {}
+
+export async function update(req: Request, res: Response) {}
+
+export async function remove(req: Request, res: Response) {}
