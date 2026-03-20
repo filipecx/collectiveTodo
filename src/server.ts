0594902e5d24fa7646d8c7c0a8f746a3d922dc2a
filipecx <filipecavalcantiximenes@gmail.com>
2026-03-20 18:12:32 -0300

    added: first routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import userRouter from './routes/userRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
+
+app.use(userRouter)
 
 app.listen(PORT, () => {
   console.log(`\n✅ Servidor Online!`);
